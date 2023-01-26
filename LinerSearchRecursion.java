beb7dfd37980bf8f0328dd15d9637d92a2c0c4a3
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-01-26 09:52:49 +0530

    Code updated

--- LinerSearchRecursion.java
+++ LinerSearchRecursion.java
@@ -11,7 +11,8 @@ class LinerSearchRecursion{
     public static void main(String args [] ){
         int a [] = {2,4,6,7,9,0,1,3,5,10};
         int key =10; 
-        int index =0;
+        int index =0; // if index starts from 0 so it will add all elements.
+    
         if(linerSearch(a,index,key))
             System.out.println(key+ " Found");
         else
