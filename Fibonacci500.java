a72d0f8e1d222a3120ddc586cceb5ca42ffe225f
Mayank Kumar Sharma <mayanksharma6979@gmail.com>
2023-04-18 04:20:57 +0530

    Finding the fibanacci of very big number

--- Fibonacci500.java
+++ Fibonacci500.java
@@ -1,3 +1,12 @@
+
+/* Mayank Kumar
+ *
+ * Import the BigInteger class from the java.math package. which can store very large int
+ * Define an array of BigInteger objects named dp, which will store the values of the Fibonacci series up to n.
+ * Initialize dp[0] to BigInteger.ZERO and dp[1] to BigInteger.ONE, since these are the first two values of the Fibonacci series.
+* Use a loop to compute the remaining values of the Fibonacci series up to n.
+* For each value i from 2 to n, compute dp[i] by adding dp[i-1] and dp[i-2].
+ */
 import java.math.BigInteger;
 public class Fibonacci500 {
     public static void main(String[] args) {
